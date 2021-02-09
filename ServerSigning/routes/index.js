d4d7a78e5711ebfe275ae7044f9892e9de1a1928
Saiju <saijumathew@gmail.com>
2021-02-09 18:29:45 -0500

    init

--- ServerSigning/routes/index.js
+++ ServerSigning/routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const Web3 = require('web3'); 
+const initPage = ()=>{
+  var Tx = require('ethereumjs-tx').Transaction;
+  const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+  web3.eth.getAccounts(console.log);
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
